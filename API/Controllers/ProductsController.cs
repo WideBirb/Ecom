8786b281827a9a3417e72e83d07a79089a526c2f
widebirb <birbguiruela@gmail.com>
2025-06-16 00:46:18 +0800

    controller ef seeding sqlite

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using API.Entities;
+using API.Data;
+
+namespace API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+
+    private readonly StoreContext _context;
+    public ProductsController ( StoreContext context )
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<List<Product>> GetProducts()
+    {
+        return _context.Products.ToList();
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Product> GetProduct( int id )
+    {
+        var product = _context.Products.Find(id);
+
+        if ( product == null ) 
+            return NotFound();
+
+        return product;
+    }
+
+}
