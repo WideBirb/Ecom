dfe6eabc36845995790969e0d65e4e22d383edc9
widebirb <birbguiruela@gmail.com>
2025-06-24 12:05:10 +0800

    s3 ep33

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,38 +1,42 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import type { Product } from "../models/product";
 import Catalog from "../../features/catalog/Catalog";
-import { Container, Typography } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
+	const [products, setProducts] = useState<Product[]>([]);
 
-  useEffect(() => {
-    fetch('https://localhost:5001/api/products')
-      .then(response => response.json())
-      .then(data => setProducts(data))
-  }, [])
+	useEffect(() => {
+		fetch("https://localhost:5001/api/products")
+			.then((response) => response.json())
+			.then((data) => setProducts(data));
+	}, []);
 
-  const addProduct = () => {
-    setProducts(prevState => [...prevState, 
-      {
-        id: prevState.length + 1,
-        name: 'product' + (prevState.length + 1), 
-        price: (prevState.length * 100) + 100,
-        quantityInStock: 100,
-        description: 'test',
-        pictureUrl: 'https://picsum.photo/200',
-        type: 'test',
-        brand: 'test'
-      }])
-  }
+	const addProduct = () => {
+		setProducts((prevState) => [
+			...prevState,
+			{
+				id: prevState.length + 1,
+				name: "product" + (prevState.length + 1),
+				price: prevState.length * 100 + 100,
+				quantityInStock: 100,
+				description: "test",
+				pictureUrl: "https://picsum.photo/200",
+				type: "test",
+				brand: "test",
+			},
+		]);
+	};
 
-
-  return (
-    <Container maxWidth='xl'>
-      <Typography variant="h4">Re-store</Typography>
-      <Catalog products={products} addProduct={addProduct}/>
-    </Container>
-  )
+	return (
+		<Container maxWidth="xl">
+			<Box display='flex' justifyContent='center' gap={3} marginY={3}>
+				<Typography variant="h4">Re-store</Typography>
+				<Button variant="contained" onClick={addProduct}>Add Product</Button>
+			</Box>
+			<Catalog products={products} />
+		</Container>
+	);
 }
 
-export default App
+export default App;
--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,21 +1,16 @@
 import { Button } from "@mui/material";
 import type { Product } from "../../app/models/product";
+import ProductList from "./ProductList";
 
 type Props = {
   products: Product[];
-  addProduct: () => void;
 }
 
-export default function Catalog({products, addProduct}: Props) {
+export default function Catalog({products}: Props) {
   return (
     <>
-      <ul>
-        {products.map((item )=> (
-          <li key={item.id}>{item.name} - {item.price}</li>
-        ))}
-      </ul>
+      <ProductList products={products}/>
       {/* // need to defer addProduct() until the button is clicked, it's being executed as soon as the component is rendered, so no ()*/}
-      <Button variant="contained" onClick={addProduct}>Add Product</Button> 
     </>
   )
 }
