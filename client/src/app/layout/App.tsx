746b63f9865df4d26c816de4149c67dc21edcbc3
widebirb <birbguiruela@gmail.com>
2025-09-02 14:10:11 +0800

    Navbar Dark Mode

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,17 +1,29 @@
 import { useEffect, useState } from "react";
 import type { Product } from "../models/product";
 import Catalog from "../../features/catalog/Catalog";
-import { Box, Button, Container, Typography } from "@mui/material";
+import { Container, createTheme, ThemeProvider, Box, CssBaseline } from "@mui/material";
+import NavBar from "./NavBar";
 
 function App() {
 	const [products, setProducts] = useState<Product[]>([]);
+	const darkMode = true;
+	const palleteType = darkMode ? 'dark' : 'light'
+	const theme = createTheme({
+		palette: {
+			mode: palleteType,
+			background: {
+				default: (palleteType === 'light') ? '#eaeaea' : '#121212'
+			}
+
+		}
+	})
 
 	useEffect(() => {
 		fetch("https://localhost:5001/api/products")
 			.then((response) => response.json())
 			.then((data) => setProducts(data));
 	}, []);
-
+/*
 	const addProduct = () => {
 		setProducts((prevState) => [
 			...prevState,
@@ -27,15 +39,24 @@ function App() {
 			},
 		]);
 	};
-
+*/
 	return (
-		<Container maxWidth="xl">
-			<Box display='flex' justifyContent='center' gap={3} marginY={3}>
-				<Typography variant="h4">Re-store</Typography>
-				<Button variant="contained" onClick={addProduct}>Add Product</Button>
-			</Box>
-			<Catalog products={products} />
-		</Container>
+		<ThemeProvider theme={theme}>
+		<CssBaseline/>
+		<NavBar/>
+		<Box
+			sx= {{
+				minHeight: '100vh',
+				background: darkMode
+					? 'radial-gradient(circle, #1e3aBa, #111B27)'
+					: 'radial-gradient(circle, #baecf9, #f0f9ff)',
+				py: 6
+			}}>
+			<Container maxWidth="xl" sx={{mt:8}}>
+				<Catalog products={products} />
+			</Container>
+		</Box>
+		</ThemeProvider>
 	);
 }
 
