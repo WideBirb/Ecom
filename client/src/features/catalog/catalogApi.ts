fec58ebc189f3729788f416d0338dca52f8e439c
widebirb <birbguiruela@gmail.com>
2025-09-06 17:58:55 +0800

    fe error handling

--- client/src/features/catalog/catalogApi.ts
+++ client/src/features/catalog/catalogApi.ts
@@ -1,6 +1,6 @@
 import { createApi } from "@reduxjs/toolkit/query/react";
 import type { Product } from "../../app/models/product";
-import { baseQueryWithErrorHandling } from "../../app/app/baseApi";
+import { baseQueryWithErrorHandling } from "../../app/api/baseApi";
 
 
 export const catalogApi = createApi({
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,10 +8,13 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './app/routes/Routes';
 import { store } from './app/store/store';
 import { Provider, useDispatch, useSelector } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
+        <ToastContainer position='bottom-right' hideProgressBar theme='colored'/>
         <RouterProvider router={router}/>
     </Provider>
   </StrictMode>,
